a0b0e2982d2171f28d2fbaedb071943edabf870a
giovanettifernando <fgiovanetti@outlook.com>
2024-02-25 16:47:37 -0300

    first commit

---
+++ operadores aritméticos.py
@@ -0,0 +1,60 @@
+a = 893 // 10
+print(a)
+
+b = 893 / 10
+print(b)
+
+c = 25.0 // 2
+print(c)
+
+d = 25.0 / 2
+print(d)
+
+e = 5678 % 1
+print(e)
+
+f = 5678 % 10
+print(f)
+
+g = 5678 % 100
+print(g)
+
+h = 5678 % 1000
+print(h)
+
+i = 5678 // 1
+print(i)
+
+j = 5678 // 10
+print(j)
+
+k = 5678 // 100
+print(k)
+
+l = 5678 // 1000
+print(l)
+
+m = 123 // 1000
+print(m)
+
+
+n = 123 / 1000
+print(n)
+
+#o = 0 / 0
+#//print(o)
+
+p = 0 ** 0
+print(p)
+
+q = 1e3 + 5
+print(q)
+
+r = 0 * 10 - 3.5
+print(r)
+
+s = 9 ** 0.5
+print(s)
+
+t = 81 ** 0.5
+print(t)
